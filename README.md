8b7e407cecdcb513a14a4157b164b7d8056b229c
Spencer Maley <swmaley02@gmail.com>
2023-12-01 21:40:42 -0600

    Added requirements.txt, requirements for project in manage.py, and updated the README file.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Miniproject4SMaley
+
+INF601 - Advanced Programming with Python </br>
+Spencer Maley
+
+## Description
+This project uses Django to create a small web app. Also this project ues Bootstrap to create a modal.
+
+## Pip install instructions
+
+Please run the following
+```
+pip install -r requirements.txt
+```
+
+## How to run
+
+In a terminal window, type to following
+```
+python manage.py runserver
+```
--- manage.py
+++ manage.py
@@ -1,3 +1,20 @@
+#INF601 - Advanced Programming in Python
+#Spencer Maley
+#Mini Project #4
+
+#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+#(5/5 points) Proper import of packages used.
+#(70/70 points) Using Django you need to setup the following:
+#(10/10 points) Setup a proper folder structure, use the tutorial as an example. You need a minimum of one app.
+#(20/20 points) You need to have a minimum of 5 pages, using a proper template structure.
+#(10/10 points) You need to have at least one page that utilizes a form.
+#(10/10 points) You need to setup Django admin and style your models for proper entry. No tight restrictions here, just make use of the admin system.
+#(10/10) You need to use Bootstrap in your web templates. I won't dictate exactly what modules you need to use but the more practice here the better. You need to at least make use of a modal.
+#(10/10) You need to setup some sort of register and login system, do some simple research to find examples.
+#(5/5 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+#(5/5 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
+#(10/10 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations. You will need to explain the steps of initializing the database and then how to run the development server for your project.
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 
